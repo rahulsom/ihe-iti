6f2406ad1e615fda2539127648cf970f9352854f
Rahul Somasunderam <rsom@certifydatasystems.com>
2013-02-27 13:28:19 -0800

    Removed some versions into variables
    Tests have been slightly enhanced

--- src/test/groovy/PdqResponseTest.groovy
+++ src/test/groovy/PdqResponseTest.groovy
@@ -1,10 +1,9 @@
 import junit.framework.TestCase
-import org.hl7.v3.EnFamily
-import org.hl7.v3.EnGiven
-import org.hl7.v3.PRPAIN201306UV02
 
 import javax.xml.bind.JAXBContext
 import javax.xml.bind.JAXBElement
+
+import org.hl7.v3.*
 
 /**
  * Tests PRPAIN201306UV02 marshaling and unmarshaling and also support for
@@ -16,13 +15,17 @@ class PdqResponseTest extends TestCase {
     JAXBContext c = JAXBContext.newInstance(PRPAIN201306UV02)
     PRPAIN201306UV02 r = c.createUnmarshaller().unmarshal(
         this.class.classLoader.getResourceAsStream('PDQV3/02_PDQQuery1Response.xml')
-    )
+    ) as PRPAIN201306UV02
+
     def patient = r.controlActProcess.subject.find{it}.registrationEvent.subject1.patient
     assert patient.providerOrganization.value.name.find{it}.content.find{it} == 'Good Health Clinic'
-
-    def nameParts = patient.patientPerson.value.name[0].content.findAll { it instanceof JAXBElement }
-    assert nameParts.find{it.value instanceof EnGiven}.value.content.find{it} == 'James'
-    assert nameParts.find{it.value instanceof EnFamily}.value.content.find{it} == 'Jones'
+    EN.metaClass.getPrefixs = { content.findAll {it instanceof JAXBElement}.findAll {it.declaredType == EnPrefix}}
+    EN.metaClass.getGivens = { content.findAll {it instanceof JAXBElement}.findAll {it.declaredType == EnGiven}}
+    PN pn = patient.patientPerson.value.name[0]
+    List<JAXBElement> nameParts = pn.content.findAll { it instanceof JAXBElement }
+    assert nameParts.find{it.declaredType == EnGiven}.value.content.find{it} == 'James'
+    assert pn.givens[0].value.content[0] == 'James'
+    assert nameParts.find{it.declaredType == EnFamily}.value.content.find{it} == 'Jones'
 
   }
 }
