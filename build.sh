0b902036e77448213bbb84d976f0b9c710dd647e
Rahul Somasunderam <rsom@certifydatasystems.com>
2013-03-12 15:37:52 -0700

    Fixed build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-if [[ "$TRAVIS_PULL_REQUEST" = false ]]; then 
+if [ "$TRAVIS_PULL_REQUEST" = false ]; then 
   python addServer.py && mvn deploy --settings ~/.m2/mySettings.xml
 else
   mvn package
