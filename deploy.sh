624b7c5e91af2c0c21104ed2c93ba6ce03e9b80d
Rahul Somasunderam <rahul.som@gmail.com>
2013-05-03 22:48:56 -0700

    Improved deploy script

--- deploy.sh
+++ deploy.sh
@@ -8,12 +8,16 @@ echo GPGKEY=$GPGKEY
 export IHEVER=$(cat pom.xml| xpath /project/version/text\(\))
 PASSPHRASE=$1
 
-if [ $PASSPHRASE = "" ]; then
+if [ "$PASSPHRASE" = "" ]; then
   echo "Passphrase is required"
   exit 1
 fi
 
-REPOCONF="-Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=sonatype-nexus-staging"
+REPOCONF="-Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ \
+    -DrepositoryId=sonatype-nexus-staging \
+    -Dgpg.passphrase=${PASSPHRASE}"
+
+mvn clean install
 
 echo mvn gpg:sign-and-deploy-file $REPOCONF -DpomFile=pom.xml -Dfile=target/ihe-iti-${IHEVER}.jar
 mvn gpg:sign-and-deploy-file $REPOCONF -DpomFile=pom.xml -Dfile=target/ihe-iti-${IHEVER}.jar
