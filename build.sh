06a5964f670b1ed3fc27f647a679b15dfdac4426
Rahul Somasunderam <rahul.som@gmail.com>
2014-01-09 19:50:34 -0800

    Error in if block of build script

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ "$TRAVIS_PULL_REQUEST" = false ]; then
-  if [ cat pom.xml | grep version | head -2 | tail -1 | cut -d\> -f2 | cut -d\< -f1 | grep SNAPSHOT ]; then
+  if [ $(cat pom.xml | grep version | head -2 | tail -1 | cut -d\> -f2 | cut -d\< -f1 | grep SNAPSHOT) ]; then
     python addServer.py && mvn deploy --settings ~/.m2/mySettings.xml
   else
     mvn package
