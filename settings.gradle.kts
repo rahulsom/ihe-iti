d34cbf855859c4773db5f83795c2626674f5ed60
Rahul Somasunderam <rahulsom@noreply.github.com>
2025-08-23 16:39:04 -0700

    fix: Reduce warnings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,11 @@
 plugins {
-  id("com.gradle.enterprise").version("3.19.2")
+  id("com.gradle.develocity").version("3.19.2")
 }
 
-gradleEnterprise {
+develocity {
   buildScan {
-    termsOfServiceUrl = "https://gradle.com/terms-of-service"
-    termsOfServiceAgree = "yes"
-    publishAlways()
+    termsOfUseUrl.set("https://gradle.com/terms-of-service")
+    termsOfUseAgree.set("yes")
   }
 }
 
