545eef7c39bbfbe8145a964b2019fbaf12bebe5e
Rahul Somasunderam <rahul.som@gmail.com>
2025-10-26 16:31:50 -0700

    chore: Use modern syntax for addPerson
    This removes the need for an unnecessary import.

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,11 +52,11 @@ description = "IHE ITI"
 
 contacts {
   validateEmails = true
-  addPerson("rahulsom@noreply.github.com", delegateClosureOf<nebula.plugin.contacts.Contact> {
+  with(addPerson("rahulsom@noreply.github.com")) {
     moniker("Rahul Somasunderam")
     roles("owner")
     github("https://github.com/rahulsom")
-  })
+  }
 }
 
 sourceSets {
